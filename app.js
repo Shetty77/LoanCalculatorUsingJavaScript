5ad5679c4a2e88d7c7224c392fa670c3ba46e2f6
shetty77 <swastikbantwal@gmail.com>
2022-05-16 17:42:53 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+document.getElementById('loan-form').addEventListener("submit",function(e){
+    document.getElementById("results").style.display="none";
+    document.getElementById("loading").style.display="block";
+    setTimeout(calculate,2000);
+    e.preventDefault();
+});
+
+function calculate(e) {
+    const amount= document.getElementById("loan_amount");
+    const intrest= document.getElementById("intrest");
+    const years= document.getElementById("years");
+    const monthlyPayment= document.getElementById("monthly_payment");
+    const totalAmount= document.getElementById("total_amount");
+    const totalIntrest= document.getElementById("total_intrest");
+
+    const principal = parseFloat(amount.value);
+    const calculatedIntrest = parseFloat(intrest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value)*12;
+    const x= Math.pow(1+calculatedIntrest, calculatedPayments);
+    const monthly =(principal*x*calculatedIntrest)/(x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalAmount.value = (monthly*calculatedPayments).toFixed(2);
+        totalIntrest.value = (monthly*calculatedPayments-principal).toFixed(2);
+        document.getElementById("results").style.display="block";
+        document.getElementById("loading").style.display="none";
+    }
+    else {
+        showAlert('Please Enter the amounts');
+    }
+    e.preventDefault();
+}
+
+function showAlert(error) {
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+    const card = document.querySelector('.card');
+    const heading = document.querySelector(".heading");
+
+    card.insertBefore(errorDiv,heading);
+    setTimeout(function() {
+        document.querySelector(".alert").remove();
+    },2000)
+
+}
